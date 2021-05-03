3f32626a54996485067998b7226bbbcdf6d333ce
BabochkinaPolina <79091702+BabochkinaPolina@users.noreply.github.com>
2021-05-03 14:31:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,68 +11,74 @@ int priority(char ch) {
     else
         return 0;
 }
+
 std::string infx2pstfx(std::string inf) {
   // добавьте сюда нужный код
   return std::string("");
-  std::stack<char> stack;
-    std::string str;
-    for (int i = 0; i < inf.length(); i++) {
-        if ((inf[i] >= '0') && (inf[i] <= '9')) {
-            str = str + inf[i];
-            str = str + " ";
-        } else if ((stack.empty()) || (inf[i] == '(')
-                 || (priority(inf[i] > priority(stack.top())))) {
-            stack.push(inf[i]);
-        } else if (inf[i] == ')') {
-            while (stack.top() != '(') {
-                str = str + stack.top();
-                str = str + ' ';
-                stack.pop();
-            }
-            stack.pop();
-        } else {
-            while (!stack.empty() && (priority(stack.top()) >= priority(inf[i]))) {
-                str = str + stack.top();
-                str = str + ' ';
-                stack.pop();
-            }
-            stack.push(inf[i]);
-        }
-    }
-     if (!stack.empty()) {
-         while (!stack.empty()) {
-            str = str + stack.top();
-            str = str + ' ';
-            stack.pop();
-         }
-     }
-    str.pop_back();
-    return str;
+  TStack<char> stack1;
+std::string result;
+for (int i = 0; i < inf.length(); ++i) {
+	if (isdigit(inf[i])) {
+		while (isdigit(inf[i])) {
+			result += inf[i];
+			i++;
+		}
+		i--;
+		result += ' ';
+	}
+	else if ((inf[i] == '(') || (stack1.isEmpty()) || (priority(inf[i]) > priority(stack1.get()))) {
+		stack1.push(inf[i]);
+	}
+	else if (inf[i] == ')') {
+		char x = stack1.get();
+		stack1.pop();
+		while (x != '(') {
+			result += x;
+			result += ' ';
+			x = stack1.get();
+			stack1.pop();
+		}
+	}
+	else {
+		while (!stack1.isEmpty()
+			&& (priority(stack1.get()) >= priority(inf[i]))) {
+			result += stack1.get();
+			result += ' ';
+			stack1.pop();
+		}
+		stack1.push(inf[i]);
+	}
+}
+while (!stack1.isEmpty()) {
+	result += stack1.get();
+	result += ' ';
+	stack1.pop();
+}
+result.pop_back();
+return result;
 }
 
 int eval(std::string pst) {
   // добавьте сюда нужный код
   return 0;
-  std::stack<char> stack;
-  int resultat;
-  for (int i = 0; i < pst.length(); i++) {
-    if ((pst[i] >= '0') && (pst[i] <= '9')) {
-      stack.push(pst[i] - '0');
-    } else if (pst[i] != ' ') {
-      int y = stack.top();
-      stack.pop();
-      int x = stack.top();
-      stack.pop();
-      if (pst[i] == '+')
-        stack.push(x + y);
-      if (pst[i] == '-')
-        stack.push(x - y);
-      if (pst[i] == '*')
-        stack.push(x * y);
-      if (pst[i] == '/')
-        stack.push(x / y);
+  std::string str;
+TStack<int> stack2;
+for (int i = 0; i < pst.length(); i++) {
+    if (pst[i] >= '0' && pst[i] <= '9') {
+        str = pst[i];
+        stack2.push(pst[i] - '0');
     }
-  }
-  resultat = stack.top();
-  return resultat;
+    else if (pst[i] != ' ') {
+        int b = stack2.get();
+        stack2.pop();
+        int a = stack2.get();
+        stack2.pop();
+        if (pst[i] == '*') stack2.push(a * b);
+        else if (pst[i] == '/') stack2.push(a / b);
+        else if (pst[i] == '+') stack2.push(a + b);
+        else
+            stack2.push(a - b);
+    }
+}
+return stack2.get();
 }
